91d0b2337b12f561994cfc665b574ee735c7e8c4
Herbert Lago <hebertpdl@gmail.com>
2022-02-16 08:31:47 -0300

    feat: add object.keys task

---
+++ data-types/keys-values-entries/2.count-properties.js
@@ -0,0 +1,12 @@
+// Write a function count(obj) that returns the number of properties in the object:
+
+let user = {
+  name: 'John',
+  age: 30
+};
+
+const count = (obj) => {
+  return Object.keys(obj).length
+}
+
+console.log( count(user) ); // 2
