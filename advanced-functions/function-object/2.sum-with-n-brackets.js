14705f3b5b8aa5ec3603f220ad7a5011d69d390a
Herbert Lago <hebertpdl@gmail.com>
2022-03-10 08:05:56 -0300

    feat: add sum with n brackets task

---
+++ advanced-functions/function-object/2.sum-with-n-brackets.js
@@ -0,0 +1,24 @@
+// Write function sum that would work like this:
+// sum(1)(2) == 3; // 1 + 2
+// sum(1)(2)(3) == 6; // 1 + 2 + 3
+// sum(5)(-1)(2) == 6
+// sum(6)(-1)(-2)(-3) == 0
+// sum(0)(1)(2)(3)(4)(5) == 15
+
+function sum(a) {
+
+  let currentSum = a;
+
+  function f(b) {
+    currentSum += b;
+    return f;
+  }
+
+  f.toString = function() {
+    return currentSum;
+  };
+
+  return f;
+}
+
+console.log(sum(1)(2))
