35a96b8e9510b0a91fe36145c4398b423e5aabcb
Herbert Lago <hebertpdl@gmail.com>
2022-03-22 08:09:11 -0300

    feat: add prototypal inheritance task

---
+++ prototypes-inheritance/prototypal-inheritance/1.searching.js
@@ -0,0 +1,26 @@
+// Given the following objects:
+
+let head = {
+  glasses: 1
+};
+
+let table = {
+  __proto__: head,
+  pen: 3
+};
+
+let bed = {
+  __proto__: table,
+  sheet: 1,
+  pillow: 2
+};
+
+let pockets = {
+  __proto__: bed,
+  money: 2000
+};
+
+// Use __proto__ to assign prototypes in a way that any property lookup will follow the path: pockets → bed → table → head. For instance, pockets.pen should be 3 (found in table), and bed.glasses should be 1 (found in head)
+
+console.log(pockets.pen)
+console.log(bed.glasses)
