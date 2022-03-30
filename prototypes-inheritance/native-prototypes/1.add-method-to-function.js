522900e46f25391e190d0e2b2d3cc7fbfaf08d9b
Herbert Lago <hebertpdl@gmail.com>
2022-03-30 08:35:37 -0300

    feat: add method to function prototype

---
+++ prototypes-inheritance/native-prototypes/1.add-method-to-function.js
@@ -0,0 +1,11 @@
+// Add to the prototype of all functions the method defer(ms), that runs the function after ms milliseconds.
+
+Function.prototype.defer = function(ms){
+  setTimeout(this, ms)
+}
+ 
+function f() {
+  console.log("Hello!");
+}
+
+f.defer(1000); // shows "Hello!" after 1 second
